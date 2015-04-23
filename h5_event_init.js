67e29ec0a0d22a287c2a5f9780f451313b84f740
LittleXiaosi <yansi1993@126.com>
2015-04-23 19:26:34 +0800

    修改点击流添加逻辑
    执行点击流函数必须先判断ping.js加载完成

--- h5_event_init.js
+++ h5_event_init.js
@@ -216,10 +216,12 @@
 
         var script2 = document.createElement('script');
         script2.setAttribute('type', 'text/javascript');
-        script2.innerHTML = "  if (typeof(pgvMain) == 'function') {pgvMain();}";
+        script2.innerHTML = " if (typeof(pgvMain) == 'function') {pgvMain();}";
 
         document.body.appendChild(script1);
-        document.body.appendChild(script2);
+        script1.onload=function(){
+            document.body.appendChild(script2);
+        }
     })();
     /*添加点击流代码 end*/
 })(window);
